a4bb9008373dfb2fb9e63683696efde13759a79a
Alejandro Bermúdez Espinosa <abermudez910827@gmail.com>
2026-07-08 21:53:57 -0500

    feat: initialize ApKuba SDK documentation site using Starlight and Astro

---
+++ astro.config.mjs
@@ -0,0 +1,103 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightThemeNova from "starlight-theme-nova";
+
+export default defineConfig({
+  site: "https://megashopcuba.github.io",
+  // base: '/apkuba-sdk',
+  outDir: "dist",
+  publicDir: "public",
+  srcDir: "src",
+  integrations: [
+    starlight({
+      title: "ApKuba SDK",
+      logo: {
+        src: "./src/assets/logo.webp",
+        replacesTitle: true,
+      },
+    
+      description:
+        "Librería Android para verificar compras y licencias en Apklis y ApKuba",
+      defaultLocale: "es",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/megashopcuba/apkuba-sdk",
+        },
+      ],
+      plugins: [
+        starlightThemeNova({
+          nav: [
+            {
+              label: "GitHub",
+              href: "https://github.com/megashopcuba/apkuba-sdk",
+            },
+            { label: "Telegram", href: "https://t.me/apkuba" },
+          ],
+        }),
+      ],
+      components: {
+        Pagination: "./src/components/CustomPagination.astro",
+      },
+      customCss: ["./src/styles/custom.css"],
+      sidebar: [
+        {
+          label: "Inicio",
+          link: "/",
+        },
+        {
+          label: "Instalación",
+          link: "/instalacion",
+        },
+        {
+          label: "Inicio Rápido",
+          link: "/inicio-rapido",
+        },
+        {
+          label: "Core",
+          items: [
+            {
+              label: "ApKuba",
+              link: "/core/apkuba",
+            },
+            {
+              label: "Apklis",
+              link: "/core/apklis",
+            },
+          ],
+        },
+        {
+          label: "Modelos",
+          items: [
+            {
+              label: "ApklisResponse",
+              link: "/modelos/apklis-response",
+            },
+            {
+              label: "Estados",
+              link: "/modelos/estados",
+            },
+          ],
+        },
+        {
+          label: "Utilidades",
+          items: [
+            {
+              label: "Validación",
+              link: "/utilidades/validacion",
+            },
+            {
+              label: "Navegación",
+              link: "/utilidades/navegacion",
+            },
+            {
+              label: "Estado de apps",
+              link: "/utilidades/aplicaciones",
+            },
+          ],
+        },
+      ],
+    }),
+  ],
+});
